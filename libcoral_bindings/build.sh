12e6aa8cbc925f1e64b94719beb913d1ce31b010
Robert Pafford <19439938+rjp5th@users.noreply.github.com>
2024-02-14 21:27:04 -0500

    Finally get C++ working with the coral Google: this is what happens when you hire using leetcode

---
+++ libcoral_bindings/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+TARGET_NAME=test_libcoral
+DOCKER_IMAGE_NAME=libcoral_build_env
+
+set -e
+
+if ! command -v docker > /dev/null; then
+    echo "Docker is not installed. Please install docker to use this script"
+    exit 1
+fi
+
+if [ -z "$(docker images -q "$DOCKER_IMAGE_NAME" 2> /dev/null)" ]; then
+    echo "Building docker image..."
+    docker build docker/ -t "$DOCKER_IMAGE_NAME"
+    exit 1
+fi
+
+docker run --rm -it -v "$(pwd):/workspace" "$DOCKER_IMAGE_NAME" /bin/bash \
+    /workspace/docker/docker_compile.sh "$TARGET_NAME" "$(id -u):$(id -g)"
---
+++ libcoral_bindings/docker/docker_compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Missing target name argument"
+    echo "Usage: ./docker_compile.sh [target] [optional new ownership]"
+    exit 1
+fi
+
+set -e
+# Add our "custom package" from the /workspace dir
+cd /libcoral
+ln -s /workspace coral/custom_package
+
+# Build the custom package target (whatever its set to in BUILD)
+bazel build --compilation_mode=opt --cpu=aarch64 --copt=-ffp-contract=off "//coral/custom_package:$1"
+
+# Copy the output artifact to the workspace directory
+mkdir -p "/workspace/out"
+cp "bazel-out/aarch64-opt/bin/coral/custom_package/$1" "/workspace/out/$1"
+chmod +w "/workspace/out/$1"
+
+# Change ownership of the output file
+if ! [ -z "$2" ]; then
+    chown -R "$2" "/workspace/out"
+fi
