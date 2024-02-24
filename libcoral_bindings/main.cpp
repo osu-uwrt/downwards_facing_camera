e100429e23900dcc23df6b4f7995efce8afce22c
Mark Fong <markcfong561@gmail.com>
2024-02-24 05:22:37 +0000

    Updated git ignore

--- libcoral_bindings/main.cpp
+++ libcoral_bindings/main.cpp
@@ -5,28 +5,32 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     auto yolo = createCoralYolo(
-        "/home/pi/yolo_model/"
-        "yolov8n-seg_full_integer_quant_edgetpu.tflite",
-        80, std::stof(argv[1]));
+        "/home/pi/robert_attempt3/yolo_models/yolov8n_seg_rescaled_edgetpu.tflite",
+        80, std::stof(argv[1]), std::stof(argv[2]));
 
-    cv::Mat image = cv::imread("/home/pi/yolo_im.bmp", cv::IMREAD_ANYCOLOR);
+    cv::Mat image = cv::imread("/home/pi/yolo_im_reshape.bmp", cv::IMREAD_ANYCOLOR);
     cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
 
+    auto start = std::chrono::high_resolution_clock::now();
     std::vector<Detection> detections = yolo->detectImage(image.data);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    printf("Took %dms\n", duration.count());
 
     std::ofstream detectionFile("Detections.csv");
 
     for (Detection &detection : detections) {
-        detectionFile << (int)detection.bbox[0] << ',';
-        detectionFile << (int)detection.bbox[1] << ',';
-        detectionFile << (int)detection.bbox[2] << ',';
-        detectionFile << (int)detection.bbox[3] << ',';
+        detectionFile << detection.bbox[0] << ',';
+        detectionFile << detection.bbox[1] << ',';
+        detectionFile << detection.bbox[2] << ',';
+        detectionFile << detection.bbox[3] << ',';
 
-        detectionFile << (int)detection.classId << ',';
-        detectionFile << (int)detection.conf << '\n';
+        detectionFile << detection.classId << ',';
+        detectionFile << detection.conf << '\n';
     }
     detectionFile.close();
 
