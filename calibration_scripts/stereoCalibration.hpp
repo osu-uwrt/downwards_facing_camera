05b6217c455896a902e5198e61e4ab57181e57f5
Mark Fong <markcfong561@gmail.com>
2024-04-16 19:59:04 -0400

    oops

--- calibration_scripts/stereoCalibration.hpp
+++ calibration_scripts/stereoCalibration.hpp
@@ -1,7 +1,7 @@
 #include <opencv2/calib3d.hpp>
 #include <tools/mySerial.h>
 
-int totalImages = 0;
+int totalImages = 41;
 cv::Size imageSize = cv::Size(640, 360);
 cv::Size patternSize = cv::Size(6, 8);
 
